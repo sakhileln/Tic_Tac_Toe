16d6fe165d0ae3d6001039316004b1f13eed1fd1
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-03 13:03:01 +0200

    Add boilerplate for test cases.

---
+++ test_tic_tac_toe.py
@@ -0,0 +1,16 @@
+"""
+A module for test cases of the tic tac toe game.
+"""
+
+import unittest
+
+
+class TestTicTacToe(unittest.TestCase):
+    """
+    Test cases for the tic tac toe game.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
