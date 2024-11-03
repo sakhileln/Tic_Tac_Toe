eb738d03ec60d0a4553f26a2acd598112a2f884e
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-03 13:36:12 +0200

    Add draw state function.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -9,3 +9,39 @@ def generate_empty_state() -> list:
     """
     state = [[0 for _ in range(3)] for _ in range(3)]
     return state
+
+
+def draw_state(state: list) -> None:
+    """
+    Draw the game board given the state.
+
+    Parameters:
+        None
+    Return:
+        None
+    """
+    print("-------------------")
+    for i in range(3):
+        print(f"|",end="")
+        for j in range(3):
+            if state[i][j] == 0:
+                piece = " "
+            elif state[i][j] == 1:
+                piece = "X"
+            elif state[i][j] == 2:
+                piece = "O"
+            # Print state
+            print(f"  {piece}  |", end="")
+        print("")
+        print("-------------------")
+
+
+test_state = [
+    [0, 2, 0],
+    [0, 1, 0],
+    [0, 0, 0]
+]
+
+
+if __name__ == "__main__":
+    draw_state(test_state)
