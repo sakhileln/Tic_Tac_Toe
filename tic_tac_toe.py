850b1377885768bf84800a9f3281a6ff129be06d
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-03 14:10:03 +0200

    Add check winner function.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -13,6 +13,27 @@ def print_board(board: list) -> None:
         print("-" * 9)
 
 
+def check_winner(board: list) -> str:
+    """
+    Checks if there is a winner.
+
+    Parameters:
+        board (int): Game board with positions
+    Return:
+        symbol (str): Winner's symbol or None
+    """
+    for row in board:
+        if row[0] == row[1] == row[2] != " ":
+            return row[0]
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != " ":
+            return board[0][col]
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return board[0][2]
+    return None
+
 
 if __name__ == "__main__":
     board = [[" " for _ in range(3)] for _ in range(3)]
