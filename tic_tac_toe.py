ac3ce7cb7f8674989a95ca886f58ecd25ba86d47
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-03 14:00:54 +0200

    Refactor the code.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,47 +1,19 @@
-def generate_empty_state() -> list:
+def print_board(board: list) -> None:
     """
-    Generate the empty game board.
+    Prints the Tic Tac Toe board.
 
     Parameters:
-        None
+        board (list): Game board with all the positions
     Return:
         None
     """
-    state = [[0 for _ in range(3)] for _ in range(3)]
-    return state
+    print("-" * 9)
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
 
-
-def draw_state(state: list) -> None:
-    """
-    Draw the game board given the state.
-
-    Parameters:
-        None
-    Return:
-        None
-    """
-    print("-------------------")
-    for i in range(3):
-        print(f"|",end="")
-        for j in range(3):
-            if state[i][j] == 0:
-                piece = " "
-            elif state[i][j] == 1:
-                piece = "X"
-            elif state[i][j] == 2:
-                piece = "O"
-            # Print state
-            print(f"  {piece}  |", end="")
-        print("")
-        print("-------------------")
-
-
-test_state = [
-    [0, 2, 0],
-    [0, 1, 0],
-    [0, 0, 0]
-]
 
 
 if __name__ == "__main__":
-    draw_state(test_state)
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    print_board(board)
