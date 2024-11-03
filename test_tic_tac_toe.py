f391f12cdf96189818b3a173cd9f264d4d1c719a
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-03 13:10:11 +0200

    Add test cases for empty state generation.

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -2,6 +2,7 @@
 A module for test cases of the tic tac toe game.
 """
 
+from tic_tac_toe import generate_empty_state
 import unittest
 
 
@@ -9,7 +10,16 @@ class TestTicTacToe(unittest.TestCase):
     """
     Test cases for the tic tac toe game.
     """
-    pass
+    def test_empty_state_generation(self):
+        """
+        Test case for an empty state
+        """
+        state = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ]
+        self.assertEqual(generate_empty_state(), state)
 
 
 if __name__ == "__main__":
