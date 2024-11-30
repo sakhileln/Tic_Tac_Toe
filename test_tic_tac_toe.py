36602ada0032b5adf6020a1bafbafc51238c42c2
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-30 09:26:35 +0200

    Refactor the code to PEP8 standards.

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -2,72 +2,54 @@
 A module for test cases of the tic tac toe game.
 """
 
+import unittest
 from tic_tac_toe import (
     create_board,
     is_board_full,
     check_winner,
 )
-import unittest
 
 
 class TestTicTacToe(unittest.TestCase):
     """
     Test cases for the tic tac toe game.
     """
+
     def test_empty_state_generation(self):
         """
         Test case for an empty board
         """
-        board = [
-            [" ", " ", " "],
-            [" ", " ", " "],
-            [" ", " ", " "]
-        ]
+        board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
         self.assertEqual(create_board(), board)
 
     def test_if_board_is_full(self):
         """
         Test case for full board.
         """
-        board = [
-            ["X", "O", "O"],
-            ["O", "O", "O"],
-            ["X", " X", "X"]
-        ]
+        board = [["X", "O", "O"], ["O", "O", "O"], ["X", " X", "X"]]
         self.assertEqual(is_board_full(board), True)
 
     def test_partially_full(self):
         """
         Test partially full board.
         """
-        board = [
-            [" ", " ", "O"],
-            ["O", "O", " "],
-            ["X", "  ", "X"]
-        ]
+        board = [[" ", " ", "O"], ["O", "O", " "], ["X", "  ", "X"]]
         self.assertEqual(is_board_full(board), False)
 
-    def test_X_winner(self):
+    def test_x_winner(self):
         """
         Test case for X as winner
         """
-        board = [
-            [" ", " ", "O"],
-            ["O", "O", " "],
-            ["X", "X", "X"]
-        ]
+        board = [[" ", " ", "O"], ["O", "O", " "], ["X", "X", "X"]]
         self.assertEqual(check_winner(board), "X")
 
-    def test_O_winner(self):
+    def test_o_winner(self):
         """
         Test case for O as winner
         """
-        board = [
-            [" ", " ", "O"],
-            ["O", "O", " "],
-            ["O", "X", "X"]
-        ]
+        board = [[" ", " ", "O"], ["O", "O", " "], ["O", "X", "X"]]
         self.assertEqual(check_winner(board), "O")
+
 
 if __name__ == "__main__":
     unittest.main()
